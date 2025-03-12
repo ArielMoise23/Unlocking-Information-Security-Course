ae8e4dfa1751c845a61d2082ac10a0e857e2edbe
ArielMoise23 <ariel.m23@gmail.com>
2025-03-12 14:37:00 +0200

    almos correct, 1 byte wormg in every test

--- lesson2_basic_cryptography/fake_RC4.py
+++ lesson2_basic_cryptography/fake_RC4.py
@@ -20,26 +20,29 @@ def get_prg(plaintext_size, k):
 
         key_copy[i], key_copy[j] = key_copy[j], key_copy[i]
 
-        keystream_index = (key_copy[j] + key_copy[i]) % key_length
+        keystream_index = (key_copy[i] + key_copy[j]) % key_length
 
-        keystream_byte = key_copy[keystream_index]
+        keystream_char = key_copy[keystream_index]
 
-        keystream.append(keystream_byte)
+        keystream.append(chr(keystream_char))
 
-
-    return keystream
+    return "".join(keystream)
 
 def fake_rc4(plaintext, keystream):
 
     if isinstance(plaintext, str):
         plaintext = [ord(c) for c in plaintext]
 
+    if isinstance(keystream, str):
+        keystream = [ord(c) for c in keystream]
+
     if len(keystream) < len(plaintext):
         raise ValueError("keystream too short")
     
     cyphertext = []
+    keystream_lst = list(keystream)
     for i in range(len(plaintext)):
-        cypher_byte = plaintext[i] ^ keystream[i]
+        cypher_byte = plaintext[i] ^ int(keystream_lst[i % len(keystream_lst)])
         cyphertext.append(cypher_byte)
 
     return bytes(cyphertext)
@@ -48,8 +51,8 @@ def fake_rc4(plaintext, keystream):
 # TEST
 if __name__ == "__main__":
     # Get the keystream
-    keystream = get_prg(10, "12345678901234567890123456789012")
-    ##output "2011336963" (string)
+    keystream = get_prg(50, "abcdefghijabcdefghijabcdefghij12")
+    encrypted1 = fake_rc4("12345678901234567890123456789012345678901234567890", keystream)
     
     ## second test - 
     keystream2 = get_prg(6, "12345678901234567890123456789012")
