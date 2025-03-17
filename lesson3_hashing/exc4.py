04435fd1d67f70f76dbbf52966ffa4eb3fc87170
ArielMoise23 <ariel.m23@gmail.com>
2025-03-17 13:50:28 +0200

    exc4

--- lesson3_hashing/exc4.py
+++ lesson3_hashing/exc4.py
@@ -15,41 +15,37 @@ def weak_md5(s):
 
 
 def find_collisions():
-    # return # return (s1, s2) such that s1 != s2 and weak_md5(s1) == weak_md5(s2)
-
     dict_of_hashes = {}
 
-
     letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"  # Alphabet string
+    found = False
+
     for first in letters:
-        is_found = check_hashing(first, dict_of_hashes)
-        if is_found:
-            return is_found
-        for second in letters:
-            is_found = check_hashing(first + second, dict_of_hashes)
-            if is_found:
-                return is_found
-            for third in letters:
-                is_found = check_hashing(first + second + third, dict_of_hashes)
-                if is_found:
-                    return is_found
-                for forth in letters:
-                    is_found = check_hashing(first + second + third + forth, dict_of_hashes)
-                    if is_found:
-                        return is_found
-                    for fifth in letters:
-                        word = first + second + third + forth + fifth
-                        is_found = check_hashing(word, dict_of_hashes)
-                        if is_found:
-                            return is_found
+        found = check_hashing(first, dict_of_hashes)
+        if found:
+            return found
+
+    count_iterations = 0
+    while not found:
+        count_iterations = count_iterations + 1
+        dict_of_hashes_copy = dict_of_hashes.copy()
+        for value in dict_of_hashes_copy.values():
+            for first in letters:
+                found = check_hashing(value + first, dict_of_hashes)
+                if found:
+                    return found
+        if count_iterations == 5:
+            found = False
 
 def check_hashing(word, dict_of_hashes):
     hash_value = weak_md5(word.encode())
 
     if hash_value in dict_of_hashes:
-        return (dict_of_hashes[hash_value], word)
+        if dict_of_hashes[hash_value] != word:
+            return (dict_of_hashes[hash_value], word)
 
     dict_of_hashes[hash_value] = word
+    return False
 
                         
-find_collisions()
+print(find_collisions())
